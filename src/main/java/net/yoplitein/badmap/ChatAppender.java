8b51f1556aca1dbe7e3ad75b4468297324fdc18b
Steven Dwy <yoplitein+git@gmail.com>
2021-08-18 17:06:50 -0700

    Color logged messages according to level in chat output

--- src/main/java/net/yoplitein/badmap/ChatAppender.java
+++ src/main/java/net/yoplitein/badmap/ChatAppender.java
@@ -9,13 +9,15 @@ import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.Property;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
 
 public class ChatAppender extends AbstractAppender
 {
-	static final boolean inDevEnv = System.getProperties().containsKey("fabric.development");
+	static final boolean inDevEnv = FabricLoader.getInstance().isDevelopmentEnvironment();
 	MinecraftServer server;
 	
 	protected ChatAppender(String name, Filter filter, Layout<? extends Serializable> layout, boolean ignoreExceptions, Property[] properties, MinecraftServer server)
@@ -32,11 +34,18 @@ public class ChatAppender extends AbstractAppender
 	@Override
 	public void append(LogEvent event)
 	{
-		if(!event.getLevel().isMoreSpecificThan(inDevEnv ? Level.DEBUG : Level.INFO)) return;
+		final var level = event.getLevel();
+		if(!level.isMoreSpecificThan(inDevEnv ? Level.DEBUG : Level.INFO)) return;
 		
 		final var playerMgr = server.getPlayerManager();
-		final var strMsg = String.format("[BadMap %5s] %s", event.getLevel().name(), event.getMessage().getFormattedMessage());
-		final var msg = new LiteralText(strMsg);
+		final var strMsg = String.format("[BadMap %s] %s", event.getLevel().name(), event.getMessage().getFormattedMessage());
+		final var msg = new LiteralText(strMsg).styled(style -> {
+			if(level == Level.DEBUG) return style.withFormatting(Formatting.GRAY, Formatting.ITALIC);
+			if(level == Level.WARN) return style.withFormatting(Formatting.YELLOW);
+			if(level == Level.ERROR) return style.withFormatting(Formatting.DARK_RED);
+			if(level == Level.FATAL) return style.withFormatting(Formatting.RED, Formatting.BOLD);
+			return style;
+		});
 		
 		for(var player: playerMgr.getPlayerList())
 			if(playerMgr.isOperator(player.getGameProfile()))
